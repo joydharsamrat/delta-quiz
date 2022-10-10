31794ca2d9db75b9ae0063190db9779de7c488de
joydharsamrat <joydharsamrat@gmail.com>
2022-10-11 04:52:07 +0600

    options added

--- src/components/Options/Options.js
+++ src/components/Options/Options.js
@@ -1,14 +1,22 @@
 import React from 'react';
 
 const Options = ({ option }) => {
+    console.log(option)
     return (
         <div className=' border p-3'>
-            <fieldset>
-                <h2>
-                    <input type="radio" name='option' id='option' />{option}
-                </h2>
-            </fieldset>
-        </div>
+
+
+
+            <p>
+                <label htmlFor={``}>
+                    <input type="radio" name='option' id='option' />
+                    {option}
+                </label>
+            </p>
+
+
+
+        </div >
     );
 };
 
--- src/components/QuizOptions/QuizOptions.js
+++ src/components/QuizOptions/QuizOptions.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import Options from '../Options/Options';
-import './QuizOptions.css'
+import './QuizOptions.css';
 
 const QuizOptions = ({ quiz }) => {
     const { question, options, correctAnswer } = quiz
     return (
-        <div className='m-5 border border-secondary rounded p-2'>
+        <div className='m-5 border border-secondary rounded p-2 text-center'>
             <h1 className='m-3'>Question : {question}</h1>
             <div className='options-container'>
                 {
-                    options.map((option, idx) => <Options key={idx} option={option}></Options>)
+                    options.map((option, idx) => <p className='border'> <label htmlFor={question.id}><input type="radio" name="option" id={question.id} />{option}</label></p>)
                 }
             </div>
         </div>
